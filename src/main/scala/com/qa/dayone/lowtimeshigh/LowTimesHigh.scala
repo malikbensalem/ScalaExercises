d676dd3b9c6790f06f306319bdf6f2dedece6ed0
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-02 17:36:02 +0000

    Hello World

---
+++ src/main/scala/com/qa/dayone/lowtimeshigh/LowTimesHigh.scala
@@ -0,0 +1,11 @@
+package com.qa.dayone.lowtimeshigh
+
+object LowTimesHigh {
+  def main(args: Array[String]): Unit = {
+    println(lowHigh(2,7,4))
+  }
+  def lowHigh(a:Int,b:Int,c:Int): Int ={
+
+    Math.min(Math.min(a,b),Math.min(a,c))+ Math.max(Math.max(Math.min(a,b),Math.min(a,c)),Math.max(Math.min(b,c),Math.min(b,a))) * Math.max(Math.max(a,b),Math.max(a,c))
+  }
+}
