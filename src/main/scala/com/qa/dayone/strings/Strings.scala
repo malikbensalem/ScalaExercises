63439b2a2c1850f0c084505b29bc3588787f5efd
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-02 17:01:13 +0000

    Hello World

--- src/main/scala/com/qa/dayone/strings/Strings.scala
+++ src/main/scala/com/qa/dayone/strings/Strings.scala
@@ -3,8 +3,20 @@ package com.qa.dayone.strings
 object Strings {
   def main(args: Array[String]): Unit = {
     println(length("hello",3))
+    println(combine("ha","llo",'a','e'))
   }
   def length(a:String,b:Int) ={
     a.substring(b-1)
+  }
+  def combine(a:String,b:String,c:Char,d:Char): String ={
+    var word = ""
+    for (i<-a+b){
+      if (i==c){
+        word+=d
+      }else{
+        word+=i
+      }
+    }
+    word
   }
 }
