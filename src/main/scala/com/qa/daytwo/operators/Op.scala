5d6c1989c380989f36b03b2e3fb9485639ceb5c5
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-03 12:01:15 +0000

    operators

---
+++ src/main/scala/com/qa/daytwo/operators/Op.scala
@@ -0,0 +1,11 @@
+package com.qa.daytwo.operators
+
+object Op {
+
+  def main(args: Array[String]): Unit = {
+    println(add(3,7)) //outputs 10
+  }
+  def add(a:Int,b:Int): Int ={
+    a+b
+  }
+}
