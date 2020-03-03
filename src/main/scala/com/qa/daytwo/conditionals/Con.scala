c537078140f8ff6c7367c4b3e2d712b895bd48c1
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-03 12:19:06 +0000

    conditional added zero condition

--- src/main/scala/com/qa/daytwo/conditionals/Con.scala
+++ src/main/scala/com/qa/daytwo/conditionals/Con.scala
@@ -4,9 +4,11 @@ object Con {
   def main(args: Array[String]): Unit = {
     println(co(2,5,true)) //outputs 7
     println(co(2,5,false)) //outputs 10
+    println(co(23,0,true)) // outputs 0
   }
   def co(a:Int,b:Int,c:Boolean): Int ={
-    if (c){a+b}
+    if (a<1|| b<1){0}
+    else if (c){a+b}
     else{a*b}
   }
 }
