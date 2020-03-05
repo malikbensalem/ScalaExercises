ed01c994f1af40aa66520130fc9353b1d72beabb
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-05 13:09:40 +0000

    BattleShip With Simple AI

--- src/main/scala/com/qa/daythree/battle/Main.scala
+++ src/main/scala/com/qa/daythree/battle/Main.scala
@@ -2,6 +2,26 @@ package com.qa.daythree.battle
 
 object Main {
   def main(args: Array[String]): Unit = {
-
+    var ai=new AI
+    var player=new Player
+    var play = true
+    ai.setOpBoard(player.myBoard)
+    player.setOpBoard(ai.myBoard)
+    println("play")
+    while (play){
+      println("Player Board:")
+      player.drawBoard
+      println("\n\n\n\n\n\n\n\nAI Board:")
+      ai.drawBoard
+      player.inputMove
+      ai.inputMove
+      if (ai.lose){
+        println("You Win!")
+        play=false
+      }else if(player.lose){
+        println("AI Wins!")
+        play=false
+      }
+    }
   }
 }
--- src/main/scala/com/qa/daythree/battle/Person.scala
+++ src/main/scala/com/qa/daythree/battle/Person.scala
@@ -5,12 +5,14 @@ abstract class Person {
   var x=0;var y=0
   var myBoard=Array.ofDim[String](12,12)
   var ships=Array(2,2,3,3,3,4,5)
+  resetBoard
+
 
   def setOpBoard(enBoard:Array[Array[String]]): Unit ={
     opBoard=enBoard
   }
   def resetBoard: Unit ={
-    var myBoard=Array(
+    myBoard=Array(
       Array(".",".",".",".",".",".",".",".",".",".",".","."),
       Array(".",".",".",".",".",".",".",".",".",".",".","."),
       Array(".",".",".",".",".",".",".",".",".",".",".","."),
@@ -24,27 +26,28 @@ abstract class Person {
       Array(".",".",".",".",".",".",".",".",".",".",".","."),
       Array(".",".",".",".",".",".",".",".",".",".",".","."),
       Array(".",".",".",".",".",".",".",".",".",".",".","."))
-    println(myBoard)
-   // myBoard(0)(0)="s";myBoard(0)(1)="s"
   }
   def placeShip: Unit ={}
   def inputMove: Unit ={}
 
   def doMove: Unit ={}
   def validate(y:Int,x:Int,shipLen:Int=0): Boolean ={
-    myBoard(0).length<y-shipLen&&myBoard.length<x-shipLen
-  }
 
-  def drawBoard: Unit ={
-    myBoard.foreach(el=>el.foreach(e=>print(e)))
-  }
-  def hitSelf(y:Int, x:Int): Unit ={
-    myBoard(y)(x)="X"
-  }
-  def lose: Unit = {
-    if (!myBoard.canEqual("@")){
-      true
+    if(myBoard(0).length-shipLen>y&&myBoard.length-shipLen>x){
+      myBoard(y)(x).equals(".")
     }else{false}
   }
 
+  def drawBoard: Unit ={}
+  def hitSelf(y:Int, x:Int): Unit ={
+    myBoard(y)(x)="X"
+  }
+  def lose: Boolean  = {
+    for (line <-myBoard) {
+      if (!line.contains("#")) {
+        true
+      }
+    }
+    false
+  }
 }
