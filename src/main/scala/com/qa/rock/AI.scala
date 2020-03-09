e29865422b961644c113b6339107a07298197ca6
Malik Bensalem <MalikBensalem@outlook.com>
2020-03-09 10:05:27 +0000

    RockPaper --template --Unfinished

--- src/main/scala/com/qa/rock/AI.scala
+++ src/main/scala/com/qa/rock/AI.scala
@@ -4,7 +4,7 @@ import scala.collection.mutable
 
 class AI extends Person {
   var learner=mutable.Map("r"->1,"p"->1,"s"->1)
-  var shortTermLearner=mutable.Map("r"->1,"p"->1,"s"->1)
+  //var shortTermLearner=mutable.Map("r"->1,"p"->1,"s"->1)
 
   def learn(syms:List[Symbol]): Unit ={
 
